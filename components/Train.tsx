194f2230ba2ec96ef3f9d5b834e90cd14d963892
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-11 00:16:14 +0200

    Modified Train component

--- components/Train.tsx
+++ components/Train.tsx
@@ -2,16 +2,18 @@ import { useEffect, useState } from 'react';
 import { Text, ScrollView, StyleSheet, View } from 'react-native';
 import Train from '../interfaces/train';
 import trainModel from '../models/trains';
+import delayedTrainsModel from '../models/delayedTrains';
+import DelayedTrain from '../interfaces/delayedTrain';
 
 
 
 function TrainList() {
     
-    const [trains, setTrains] = useState<Train[]>([]);
+    const [trains, setTrains] = useState<DelayedTrain[]>([]);
     
     useEffect( () => {
         (async () => {
-            setTrains(await trainModel.getTrains());
+            setTrains(await delayedTrainsModel.getDelayedTrains());
             /* console.log(trains);
             for (const train of trains) {
                 if (train.FromLocation !== undefined) {
@@ -26,17 +28,11 @@ function TrainList() {
    
 
     const list = trains.map((train, index) => 
+    <Text key={index}>
+        {train.AdvertisedTrainIdent} - {train.FromLocationName} : {train.FromLocation ? train.FromLocation[0] : ""}, {train.FromLocation ? train.FromLocation[1] : ""} till  {train.ToLocationName} : {train.ToLocation ? train.ToLocation[0] : ""}, {train.ToLocation ? train.ToLocation[1] : ""}
+    </Text>
     
-    <View key={index}>
-        
-        <Text>
-            { train.AdvertisedTrainIdent} 
-            { train.FromLocation !== undefined && train.FromLocation[0].LocationName } - 
-            { train.ToLocation !== undefined && train.ToLocation[0].LocationName } - 
-            { train.EstimatedTimeAtLocation}
-        </Text>
-
-    </View>);
+    );
     //const list = trains.map((train, index) => <Text key={index}>{ train.advertisedTrainIdent}</Text>);
     //console.log(`Lista: ${list}`);
      return (

e0fbff5ec6bb14adf80d7cf0e15c9a6867579c51
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-10 19:54:03 +0200

    Added screens for trains and map

--- App.tsx
+++ App.tsx
@@ -1,12 +1,39 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Home from './components/Home';
+import TrainMap from './components/TrainMap';
+
+
+const Tab = createBottomTabNavigator();
+
+const routeIcons = {
+  "Tåg": "train",
+  "Karta": "map"
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName = routeIcons[route.name] || "alert";
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+          tabBarInactiveTintColor: "grey",
+          tabBarActiveTintColor: "blue",
+        })}
+        >
+          <Tab.Screen name="Tåg" component={Home}/>
+          <Tab.Screen name="Karta" component={TrainMap}/>
+        </Tab.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +41,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });

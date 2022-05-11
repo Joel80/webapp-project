c059547bc1b44bc27a4e63f24117ecfaa1d2564a
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-12 00:40:11 +0200

    Added lat och long on model

--- models/delayedTrains.ts
+++ models/delayedTrains.ts
@@ -3,6 +3,7 @@ import stationModel from './stations';
 import delayedTrainInterface from '../interfaces/delayedTrain';
 import Train from '../interfaces/train';
 import Station from '../interfaces/station';
+import { EnumNumberMember } from '../../../../../../../../../mnt/c/Users/jdtlo/dbwebb-kurser/webapp/me/kmom10/proj/node_modules/@babel/types/lib';
 
 
 //Usable regex:  /(-\d+|\d+)(,\d+)*(\.\d+)*/g or (/(\d+)(\.\d+)/g)
@@ -28,7 +29,17 @@ const trains = {
 
             let trainFromCoords: RegExpMatchArray | null = [];
 
+            let trainFromLat: number = 0.0;
+
+            let trainFromLong: number = 0.0;
+            
             let trainToCoords: RegExpMatchArray | null = [];
+
+            let trainToLat: number = 0.0;
+
+            let trainToLong: number = 0.0;
+
+
 
 
             for (const station of stations) {
@@ -36,7 +47,14 @@ const trains = {
                     if (station.LocationSignature === train.FromLocation[0].LocationName) {
                         trainFromLocationName = station.AdvertisedLocationName;
                         trainFromCoords = station.Geometry.WGS84.match((/(\d+)(\.\d+)/g));
-                        
+                        if (trainFromCoords && parseFloat(trainFromCoords[0]) !== NaN && parseFloat(trainFromCoords[1]) != NaN) {
+                            trainFromLong = parseFloat(trainFromCoords[0]);
+                            trainFromLat = parseFloat(trainFromCoords[1]);
+                            
+                        } else {
+                            trainFromLat = 0.0;
+                            trainFromLong = 0.0;
+                        }
                     }
                 }
 
@@ -44,6 +62,13 @@ const trains = {
                     if (station.LocationSignature === train.ToLocation[0].LocationName) {
                         trainToLocationName = station.AdvertisedLocationName;
                         trainToCoords = station.Geometry.WGS84.match((/(\d+)(\.\d+)/g));
+                        if (trainToCoords && parseFloat(trainToCoords[0]) !== NaN && parseFloat(trainToCoords[1]) != NaN) {
+                            trainToLong = parseFloat(trainToCoords[0]);
+                            trainToLat = parseFloat(trainToCoords[1]);
+                        } else {
+                            trainToLat = 0.0;
+                            trainToLong = 0.0;
+                        }
                     }
                 }               
             }
@@ -58,7 +83,11 @@ const trains = {
                 FromLocationName: trainFromLocationName,
                 ToLocationName: trainToLocationName,
                 FromLocation: trainFromCoords,
-                ToLocation: trainToCoords
+                ToLocation: trainToCoords,
+                FromLat: trainFromLat,
+                FromLong: trainFromLong,
+                ToLat: trainToLat,
+                ToLong: trainToLong
 
             };
 

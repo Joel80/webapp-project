a2713254b2ad380f6dad52322c1aa5f0d53f95f2
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-12 22:06:07 +0200

    Added properties for DelayedBy to model

--- models/delayedTrains.ts
+++ models/delayedTrains.ts
@@ -3,7 +3,6 @@ import stationModel from './stations';
 import delayedTrainInterface from '../interfaces/delayedTrain';
 import Train from '../interfaces/train';
 import Station from '../interfaces/station';
-import { EnumNumberMember } from '../../../../../../../../../mnt/c/Users/jdtlo/dbwebb-kurser/webapp/me/kmom10/proj/node_modules/@babel/types/lib';
 
 
 //Usable regex:  /(-\d+|\d+)(,\d+)*(\.\d+)*/g or (/(\d+)(\.\d+)/g)
@@ -21,7 +20,21 @@ const trains = {
 
         const stations: Station[] = await stationModel.getStations();
 
+        const markedTrains: string[] = [];
+
         for (const train of trains) {
+            
+            // Skip trains that are already accounted for (a train can appear several times
+            // because the data in the API contains the same train with delay info for different
+            // stations between fromLocation and toLocation, in this solution we are only counting the 
+            // "first" ecountered delay)
+            if (markedTrains.includes(train.AdvertisedTrainIdent)) {
+                continue;
+            }
+
+            markedTrains.push(train.AdvertisedTrainIdent);
+
+            //console.log(train.AdvertisedTrainIdent);
 
             let trainFromLocationName: string = "";
 
@@ -29,33 +42,44 @@ const trains = {
 
             let trainFromCoords: RegExpMatchArray | null = [];
 
-            let trainFromLat: number = 0.0;
+            let trainFromLat: number  = -0.0;
 
-            let trainFromLong: number = 0.0;
+            let trainFromLong: number  = -0.0;
             
             let trainToCoords: RegExpMatchArray | null = [];
 
-            let trainToLat: number = 0.0;
+            let trainToLat: number  = -0.0;
 
-            let trainToLong: number = 0.0;
+            let trainToLong: number  = -0.0;
 
+            let trainDelayedBy: number = 0;
 
+            let walkingDistance: number = 0;
 
-
+           
             for (const station of stations) {
                 if (train.FromLocation !== undefined) {
                     if (station.LocationSignature === train.FromLocation[0].LocationName) {
+                        //console.log(station.LocationSignature, train.AdvertisedTrainIdent);
                         trainFromLocationName = station.AdvertisedLocationName;
                         trainFromCoords = station.Geometry.WGS84.match((/(\d+)(\.\d+)/g));
+
                         if (trainFromCoords && parseFloat(trainFromCoords[0]) !== NaN && parseFloat(trainFromCoords[1]) != NaN) {
                             trainFromLong = parseFloat(trainFromCoords[0]);
                             trainFromLat = parseFloat(trainFromCoords[1]);
                             
                         } else {
-                            trainFromLat = 0.0;
-                            trainFromLong = 0.0;
+                            trainFromLat = -0.0;
+                            trainFromLong = -0.0;
                         }
                     }
+                    
+                    let dateEstimated = new Date(train.EstimatedTimeAtLocation);
+                    
+                    let dateAdvertised = new Date(train.AdvertisedTimeAtLocation);
+
+                    trainDelayedBy = (dateEstimated.valueOf() - dateAdvertised.valueOf()) / 60000;
+
                 }
 
                 if (train.ToLocation !== undefined) {
@@ -66,8 +90,8 @@ const trains = {
                             trainToLong = parseFloat(trainToCoords[0]);
                             trainToLat = parseFloat(trainToCoords[1]);
                         } else {
-                            trainToLat = 0.0;
-                            trainToLong = 0.0;
+                            trainToLat = -0.0;
+                            trainToLong = -0.0;
                         }
                     }
                 }               
@@ -87,11 +111,14 @@ const trains = {
                 FromLat: trainFromLat,
                 FromLong: trainFromLong,
                 ToLat: trainToLat,
-                ToLong: trainToLong
-
+                ToLong: trainToLong,
+                DelayedBy: trainDelayedBy, 
             };
-
-            delayedTrainsArray.push(delayedTrain);
+            // Only push delayed trains with a from latitude to array
+            if (delayedTrain.FromLat !== -0.0) {
+                delayedTrainsArray.push(delayedTrain);
+            }
+           
             
         }
 

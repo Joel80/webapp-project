e1ccd3cfc28afe24bf6c7161c1e60d5e29921e82
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-14 13:23:28 +0200

    New functions to get stations by location signatures and get user data about favorite stations

--- models/stations.ts
+++ models/stations.ts
@@ -1,4 +1,6 @@
 import config from '../config/config.json';
+import authConfig from '../config/authConfig.json';
+import storage from "./storage";
 
 const trains = {
     getStations: async function getStations() {
@@ -9,6 +11,50 @@ const trains = {
 
         return result.data;
     },
+    getStationsByLocationSignatures: async function getStationsByLocationSignatures(signatures: string[]) {
+        console.log("Calling getStationsByLocationSignatures");
+        console.log(signatures);
+        const response = await fetch(`${config.base_url}/stations`);
+
+        const result = await response.json();
+
+        //console.log(result);
+
+        const stations = result.data;
+
+        const matchingStations = [];
+
+
+        for (const signature of signatures) {
+            for (const station of stations) {
+                if (station.LocationSignature === signature) {
+
+                    console.log(station);
+                    matchingStations.push(station);
+                }
+            }
+        }
+
+        //console.log(stations);
+
+        return matchingStations;
+
+    },
+    getFavoriteStationsData: async function getFavoriteStationsData() {
+        console.log("Getting invoices");
+        const tokenAndData = await storage.readToken();
+        const token = tokenAndData.token;
+        console.log(`token: ${token}`)
+        const response = await fetch(`${authConfig.base_url}/invoices?api_key=${authConfig.api_key}`, {
+            headers: {
+                'x-access-token': token
+            }
+        });
+        const result = await response.json();
+
+        console.log(`data: ${result.data}`);
+        return result.data;
+    }
 };
 
 export default trains;

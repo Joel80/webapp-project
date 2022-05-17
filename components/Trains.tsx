d073974c54c72ecdfa619a5eef2d27f54ea5a172
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-18 00:08:59 +0200

    Worked on table style, added icon for train

--- components/Trains.tsx
+++ components/Trains.tsx
@@ -6,6 +6,7 @@ import delayedTrainsModel from '../models/delayedTrains';
 import delayedTrain from '../interfaces/delayedTrain';
 import { DataTable } from 'react-native-paper';
 import { Appbar } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
 
 function zeroPad(number: number): string {
     if (number < 10) {
@@ -39,24 +40,22 @@ function TrainList() {
         return (
             
         <View key={index}>
-            <DataTable.Row style={{height: 75}}>
-                <DataTable.Cell style={{flex: 0}} textStyle={{fontWeight: 'bold', fontSize: 18}}>
-                    {train.AdvertisedTrainIdent} {train.FromLocationName} 
+            <DataTable.Row style={{height: 50, marginTop: 0, marginBottom: 0, borderBottomWidth: 0, paddingBottom: 0}}>
+                <DataTable.Cell style={{flex: 0}} textStyle={{fontWeight: 'bold', fontSize: 17}}>
+                    {train.AdvertisedTrainIdent}  {train.FromLocationName}
                 </DataTable.Cell>
-                <DataTable.Cell style={{flex: 1}} textStyle={{fontWeight: 'bold', fontSize: 18}}>
-                    
+                <DataTable.Cell style={{flex: 1}} textStyle={{fontWeight: 'bold', fontSize: 17}}>
                 </DataTable.Cell>
-                <DataTable.Cell style={{flex: 0}} textStyle={{fontWeight: 'bold', fontSize: 18,  padding:10}} numeric>
-                    {estimatedTime}
+                <DataTable.Cell style={{flex: 0}} textStyle={{color: "#959595", textDecorationLine: 'line-through', fontWeight: 'bold', fontSize: 17,  padding:10}} numeric>
+                   {advertisedTime}
                 </DataTable.Cell >
-                <DataTable.Cell style={{flex: 0}} textStyle={{textDecorationLine: 'line-through', color: 'red', fontWeight: 'bold', fontSize: 18}} numeric>
-                    {advertisedTime}
+                <DataTable.Cell style={{flex: 0}} textStyle={{color: 'red', fontWeight: 'bold', fontSize: 17}} numeric>
+                     {estimatedTime}
                 </DataTable.Cell>
+            </DataTable.Row >
+            <DataTable.Row style={{borderBottomWidth: 1, borderBottomColor: "#ececed", minHeight:0, paddingBottom: 10, paddingTop: 0, marginTop: 0}}>
+                <DataTable.Cell ><Ionicons name="train" color={"#217cff"} size={14}/> Tåg </DataTable.Cell>
             </DataTable.Row>
-            
-            <DataTable.Cell>
-                <Text>  </Text> 
-            </DataTable.Cell>
         </View> )
     
     });

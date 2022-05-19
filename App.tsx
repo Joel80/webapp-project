ddd4297b8dd4c187a68ec6b6826b332fb6fc712a
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-20 00:31:22 +0200

    Cleaning up

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet} from 'react-native';
+import { StyleSheet, Button} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Train from './components/train/Train';
 import TrainMap from './components/TrainMap';
@@ -56,71 +56,33 @@ export default function App() {
 
    return (
     <PaperProvider theme={theme}>
-      <SafeAreaView style={styles.container}>
-        <NavigationContainer>
-          <Tab.Navigator screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size }) => {
-              let iconName = routeIcons[route.name] || "alert";
-              return <Ionicons name={iconName} size={size} color={color} />
-            },
-            tabBarInactiveTintColor: "grey",
-            tabBarActiveTintColor: "#217cff",
-          })}
-          >
-            <Tab.Screen name="Tåg" component={Train}
-                 
-                 options={ 
-                  {
-                      /* header: () => <TrainViewHeader />, */
-                      //headerStyle: {},
-                      //headerTitle: "Tågförseningar", 
-                      headerShown: false,
-                      //headerTitleAlign: "left",
-                      //headerTitleStyle: {fontSize: 18, fontWeight: 'bold'},
-                      
-                      /* headerRight: () => 
-                      <View style={{flexDirection: 'row'}} >
-                          <View>
-                              <Ionicons name="navigate-outline" color={"#217cff"} size={25}/>
-                          </View>
-
-                          <View style={{marginLeft: 20}}>
-                              <Ionicons  name="list-outline" color={"#217cff"} size={25} onPress={() => setModalVisible(true)}/>
-                          </View>
-                      </View>
-                      , */
-                      //headerRightContainerStyle: {margin: 1, paddingRight: 15, paddingBottom: 15},
-
-                      /* headerLeft: () => 
-                      <View style={{flexDirection: 'row'}} >
-                          <View>
-                              <Ionicons name="heart-outline" color={"#217cff"} size={25}/>
-                          </View>
-                      </View>
-                      ,
-                       *///headerLeftContainerStyle: {margin: 1, paddingLeft: 15, paddingBottom: 15},
-                      
-                      
-                  }                    
-              }
-            />
-            
-            <Tab.Screen name="Karta" component={TrainMap}
-                options={{headerShown: false,}}
-            />
-            {isLoggedIn ?
-                <Tab.Screen name="Stationer">
-                  { () => <Station setIsLoggedIn={setIsLoggedIn}/>}
-                </Tab.Screen> :
-                <Tab.Screen name="Logga in">
-                  { () => <Auth setIsLoggedIn={setIsLoggedIn}/>}
-                </Tab.Screen>
-            }
-          </Tab.Navigator>
-        </NavigationContainer>
-        <StatusBar style="auto" />
-        <FlashMessage position="top" />
-      </SafeAreaView>
+        <SafeAreaView style={styles.container}>
+            <NavigationContainer>
+                <Tab.Navigator screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color, size }) => {
+                        let iconName = routeIcons[route.name] || "alert";
+                        return <Ionicons name={iconName} size={size} color={color} />
+                        },
+                        tabBarInactiveTintColor: "grey",
+                        tabBarActiveTintColor: "#217cff",
+                    })}
+                >
+                    <Tab.Screen name="Tåg" component={Train}  options={{headerShown: false}}/>
+                    
+                    <Tab.Screen name="Karta" component={TrainMap} options={{headerShown: false,}}/>
+                    {isLoggedIn ?
+                        <Tab.Screen name="Stationer" options={{headerShown: false}}>
+                            { () => <Station setIsLoggedIn={setIsLoggedIn}/>}
+                        </Tab.Screen> :
+                        <Tab.Screen name="Logga in">
+                            { () => <Auth setIsLoggedIn={setIsLoggedIn}/>}
+                        </Tab.Screen>
+                    }
+                </Tab.Navigator>
+            </NavigationContainer>
+            <StatusBar style="auto" />
+            <FlashMessage position="top" />
+        </SafeAreaView>
     </PaperProvider>
   );
 }

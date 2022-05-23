6150d0ae36a39fa5201a87bd73bb1c106a61f5f4
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-23 23:40:29 +0200

    Added type for StackParamList

--- components/train/Train.tsx
+++ components/train/Train.tsx
@@ -1,52 +1,72 @@
-import { Text, View, Modal, Pressable, Button } from 'react-native';
+import { View, Button } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TrainList from './TrainList';
 import TrainsAtStation from './TrainsAtStation';
-//Change to modal from stations
 import StationModal from './StationModal';
 import { Ionicons } from '@expo/vector-icons';
-import { useState } from 'react';
+import stationInterface from '../../interfaces/station';
+import type { RouteProp } from '@react-navigation/native';
 
+/**Navigator for train views */
 
-const Stack = createNativeStackNavigator();
+// Type for paramlist for navigator
+type StackParamList ={
+    List: {
+        name: string,
+    };
+
+    StationModal: {
+        name: string,
+    };
+
+    TrainsAtStation: {
+        station: stationInterface,
+        name: string,
+    };
+
+}
+
+type TrainsAtStationRouteProp = RouteProp<StackParamList, 'TrainsAtStation'>
+
+const Stack = createNativeStackNavigator<StackParamList>();
 
 export default function Train() {
 
     return (
         <Stack.Navigator initialRouteName="List">
             <Stack.Screen name="List" component={TrainList}
-                options={ ({navigation}) =>({
+                options={ ({navigation}) =>
+                    ({
                     
-                    /* header: () => <TrainViewHeader />, */
-                        headerStyle: {},
                         headerTitle: "Tågförseningar", 
                         headerShown: true,
-                        //headerTitleAlign: "left",
-                        //headerTitleStyle: {fontSize: 18, fontWeight: 'bold'},
-                        
+                                        
                         headerRight: () => 
                         <View style={{flexDirection: 'row'}} >
                             <View>
                                 <Ionicons name="navigate-outline" color={"#217cff"} size={25}/>
                             </View>
-  
+
                             <View style={{marginLeft: 20}}>
-                                <Ionicons  name="list-outline" color={"#217cff"} size={25} onPress={(screenProps) => navigation.navigate('StationModal', {navigation: {navigation}})}/>
+                                <Ionicons  
+                                    name="list-outline" 
+                                    color={"#217cff"} 
+                                    size={25} 
+                                    onPress={(screenProps) => navigation.navigate('StationModal', {navigation: {navigation}})}
+                                />
                             </View>
-                        </View>
-                        ,
-                        //headerRightContainerStyle: {margin: 1, paddingRight: 15, paddingBottom: 15},
-  
+                        </View>,
+                    
                         headerLeft: () => 
                         <View style={{flexDirection: 'row'}} >
                             <View>
                                 <Ionicons name="heart-outline" color={"#217cff"} size={25}/>
                             </View>
-                        </View>
-                        ,
-                        //headerLeftContainerStyle: {margin: 1, paddingLeft: 15, paddingBottom: 15},
+                        </View>,
+               
 
-                })}
+                    })
+                }
             
             />         
             <Stack.Screen name="StationModal" component={StationModal}
@@ -55,8 +75,6 @@ export default function Train() {
                         presentation: "modal",
                         headerTitle: "Stationer", 
                         headerShown: true,
-                        //headerTitleAlign: "left",
-                        //headerTitleStyle: {fontSize: 18, fontWeight: 'bold'},
                         
                         headerLeft: () =>
                         <View> 
@@ -66,18 +84,19 @@ export default function Train() {
                         </View>
                     })}
             />
-            <Stack.Screen name="TrainsAtStation" component={TrainsAtStation}
-                    
-                    options={ ({route, navigation}) =>({ 
-                        title: route?.params?.station.AdvertisedLocationName,
-                        headerLeft: () =>
-                        <View> 
-                            
-                            <Button title="Tillbaka" onPress={(screenProps) => navigation.navigate('List')}/>
-    
-                        </View>
-                    
-                    })}
+            <Stack.Screen name="TrainsAtStation" 
+                component={TrainsAtStation}
+                              
+                options={ ({route, navigation}) =>({ 
+                    title: route?.params?.station.AdvertisedLocationName,
+                    headerLeft: () =>
+                    <View> 
+                        
+                        <Button title="Tillbaka" onPress={(screenProps) => navigation.navigate('List')}/>
+
+                    </View>
+                
+                })}
             />
         </Stack.Navigator>  
     );

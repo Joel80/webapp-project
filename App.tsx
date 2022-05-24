9271e8e992afc53d6aac40428714cddd6aaa30bd
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-24 23:45:56 +0200

    Cleaning up code

--- App.tsx
+++ App.tsx
@@ -11,7 +11,6 @@ import Auth from './components/auth/Auth';
 import { useState, useEffect } from 'react';
 import authModel from './models/auth';
 import FlashMessage from 'react-native-flash-message';
-import { DefaultTheme, Provider as PaperProvider, Appbar } from 'react-native-paper';
 
 
 const Tab = createBottomTabNavigator();
@@ -24,66 +23,44 @@ const routeIcons = {
   "Logga in": "lock-closed"
 }
 
-const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  dark: true,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#fff',
-    accent: '#f1c40f',
-  },
-};
-
-function appBarHeader() {
-  return (
-    <Appbar.Header>
-      {/* <Appbar.Content title="Tåg" /> */}
-      <Appbar.Action color='#217cff' icon="menu"  />
-    </Appbar.Header>
-  );
-}
-
-
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState<Boolean>(false);
   useEffect(() => {
     (async () => {
-      setIsLoggedIn(await authModel.loggedIn())
+      setIsLoggedIn(await authModel.loggedIn());
     })();   
   }, []);
 
    return (
-    <PaperProvider theme={theme}>
-        <SafeAreaView style={styles.container}>
-            <NavigationContainer>
-                <Tab.Navigator screenOptions={({ route }) => ({
-                    tabBarIcon: ({ color, size }) => {
-                        let iconName = routeIcons[route.name] || "alert";
-                        return <Ionicons name={iconName} size={size} color={color} />
-                        },
-                        tabBarInactiveTintColor: "grey",
-                        tabBarActiveTintColor: "#217cff",
-                    })}
-                >
-                    <Tab.Screen name="Tåg" component={Train}  options={{headerShown: false}}/>
-                    
-                    <Tab.Screen name="Karta" component={TrainMap} options={{headerShown: false,}}/>
-                    {isLoggedIn ?
-                        <Tab.Screen name="Stationer" options={{headerShown: false}}>
-                            { () => <Station setIsLoggedIn={setIsLoggedIn}/>}
-                        </Tab.Screen> :
-                        <Tab.Screen name="Logga in" options={{headerShown: false}}>
-                            { () => <Auth setIsLoggedIn={setIsLoggedIn}/>}
-                        </Tab.Screen>
-                    }
-                </Tab.Navigator>
-            </NavigationContainer>
-            <StatusBar style="auto" />
-            <FlashMessage position="top" />
-        </SafeAreaView>
-    </PaperProvider>
+
+    <SafeAreaView style={styles.container}>
+        <NavigationContainer>
+            <Tab.Navigator screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    let iconName = routeIcons[route.name] || "alert";
+                    return <Ionicons name={iconName} size={size} color={color} />
+                    },
+                    tabBarInactiveTintColor: "grey",
+                    tabBarActiveTintColor: "#217cff",
+                })}
+            >
+                <Tab.Screen name="Tåg" component={Train}  options={{headerShown: false}}/>
+                
+                <Tab.Screen name="Karta" component={TrainMap} options={{headerShown: false,}}/>
+                {isLoggedIn ?
+                    <Tab.Screen name="Stationer" options={{headerShown: false}}>
+                        { () => <Station setIsLoggedIn={setIsLoggedIn}/>}
+                    </Tab.Screen> :
+                    <Tab.Screen name="Logga in" options={{headerShown: false}}>
+                        { () => <Auth setIsLoggedIn={setIsLoggedIn}/>}
+                    </Tab.Screen>
+                }
+            </Tab.Navigator>
+        </NavigationContainer>
+        <StatusBar style="auto" />
+        <FlashMessage position="top" />
+    </SafeAreaView>
   );
 }
 
